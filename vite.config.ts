7d16cd80479615075b7ccbd537a69e842c7df39c
RKamey <ralonso@ucol.mx>
2025-01-13 00:32:40 -0600

    refactor: Update project configuration and structure
    - Removed tsconfig.app.json and tsconfig.node.json files.
    - Updated tsconfig.json to include new compiler options.
    - Modified package.json to remove private flag and add new fields for module exports.
    - Updated Vite configuration to include TypeScript declaration generation.
    - Refactored App component to use new Button and Typography components from Ant Design, enhancing UI structure.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ rollupTypes: true })],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'kamey-ui',
+      fileName: (format) => `kamey-ui.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'jsxRuntime',
+        },
+      },
+    },
+  },
 })
