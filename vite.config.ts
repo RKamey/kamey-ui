434e8e61bee356015c573718ec8387ad1373c41f
RKamey <ralonso@ucol.mx>
2025-01-15 15:02:43 -0600

    chore: update package.json and vite.config.ts to improve module exports and build configuration, including changes to main entry point and plugin options

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import dts from 'vite-plugin-dts';
 import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [react(), dts({ rollupTypes: true })],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
